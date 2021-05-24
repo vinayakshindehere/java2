0ed9ae8678fda0d31cd18811973dfd4d9f9bf2ec
manishashah <Vinayak.Shinde@XEBIAINDIA.LOCAL>
2021-05-24 12:08:51 +0530

    first commit

---
+++ Exception.java
@@ -0,0 +1,13 @@
+package javaPrograms;
+
+public class Exception {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+System.out.println("hi how are u");
+System.out.println("third commit")
+
+	}
+
+}
